4dd6d8129ab4979ec2eaf13e6931f0c8d301a6c6
Prashant Kumar <prashant.kumar@edvanta.com>
2025-12-23 21:59:57 +0530

    fix: add basename to router for github pages support

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pk-s-kinetic-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
